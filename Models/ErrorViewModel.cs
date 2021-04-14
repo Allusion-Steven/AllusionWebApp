f4477835fb72ed9b9acbaef4df379fe54b38f5a2
steven jenkins <steven@allusionsoftware.onmicrosoft.com>
2021-04-14 16:38:04 -0400

    imports

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AllusionWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
